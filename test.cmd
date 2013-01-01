1de54c1e09047452a2e89ffa00d37635883eda4c
Katie Gietzen <goddessartemis85@gmail.com>
2013-01-01 15:58:58 -0800

    Fixed the test script.

--- test.cmd
+++ test.cmd
@@ -1,8 +1,9 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+    set tests=
     pushd test
-        for /r %%i in (*.js) do set TESTS=%TESTS% "%%i"
-        for /r %%i in (*.coffee) do set TESTS=%TESTS% "%%i"
+        for /r %%i in (*.js) do set tests=!tests! "%%i"
+        for /r %%i in (*.coffee) do set tests=!tests! "%%i"
     popd
-    mocha --compilers coffee:coffee-script %TESTS%
+    mocha --compilers coffee:coffee-script %tests%
 endlocal
