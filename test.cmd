bb56251c968787c6e9b25887d9686ff8f597448c
Katie Gietzen <goddessartemis85@gmail.com>
2013-01-01 19:20:20 -0800

    Changed test script to allow for specifying individual files or folders.

--- test.cmd
+++ test.cmd
@@ -1,9 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
-    set tests=
-    pushd test
-        for /r %%i in (*.js) do set tests=!tests! "%%i"
-        for /r %%i in (*.coffee) do set tests=!tests! "%%i"
-    popd
+    set tests=%*
+    if "%tests%"=="" (
+        pushd test
+            for /r %%i in (*.js) do set tests=!tests! "%%i"
+            for /r %%i in (*.coffee) do set tests=!tests! "%%i"
+        popd
+    )
     mocha --compilers coffee:coffee-script %tests%
 endlocal
